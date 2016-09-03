b860b9cb0d937881305be2049e191c6506141d72
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-09-03 04:51:22 +0200

    fixed it :smirk:

--- gameoflife.rb
+++ gameoflife.rb
@@ -4,58 +4,104 @@
 #
 # Distributed under terms of the GPLv3 license.
 
-SIZE = 20
+SIZE = 70
 
 # main class that contains all logic
 class GameOfLife
   def initialize
     @board = Array.new(SIZE) { Array.new(SIZE, 0) }
-    @board[0][0] = 1
-    @board[1][0] = 1
-    @board[1][1] = 1
-    @board[0][1] = 1
-    @board[2][0] = 1
-    @board[2][2] = 1
-    @board[0][2] = 1
-    @board[2][SIZE-1] = 1
+
+    # Acorn preset, really dope
+    @board[20][20] = 1
+    @board[21][20] = 1
+    @board[21][18] = 1
+    @board[23][19] = 1
+    @board[24][20] = 1
+    @board[25][20] = 1
+    @board[26][20] = 1
   end
 
   def show
     @board.transpose.each do |row|
-      row.each { |cell| print cell }
+      row.each { |cell| print cell == 1 ? '#' : '.' }
       print "\n"
     end
   end
 
   def neighbours_coords(pos_x, pos_y)
-    coord = [pos_x, pos_y]
+    coords = []
 
-    # loop through coords
-    [0, 1].map do |pos|
-      # loop through add/subtract 1
-      [1, -1].map do |diff|
-        # if x-axis; add/sub 1 to x-axis and use y as normal and etc.
-        pos.zero? ? [coord[0] + diff, coord[1]] : [coord[0], coord[1] + diff]
-      end
-    end.flatten.each_slice(2) # get coords in arrs of two
+    [
+      [-1, -1], [0, -1], [1, -1],
+      [-1, 0],           [1, 0],
+      [-1, 1],  [0, 1],  [1, 1]
+    ].each do |pos|
+      coords << [pos_x + pos[0], pos_y + pos[1]]
+    end
+
+    coords
   end
 
   # return the sum of alive neighbors
   def neighbours(pos_x, pos_y)
-    neighbours_coords(pos_x, pos_y).map do |coord|
-      if coord.any? { |c| c < 0 || c > SIZE }
-        0
-      else
-        @board[coord[0]][coord[1]]
-      end
-    end.inject(:+) # sum
+    cells = neighbours_coords(pos_x, pos_y).flatten.each_slice(2).map do |c|
+      next if c.any? { |coord| coord < 0 || coord > SIZE - 1 }
+      cell(c[0], c[1])
+    end
+
+    cells.delete_if(&:nil?)
+
+    cells.nil? ? 0 : cells.inject(0, :+)
+  end
+
+  # get value of cell
+  def cell(pos_x, pos_y)
+    @board[pos_x][pos_y]
   end
 
   # determine whether the cell should live or die
   def live_or_die(pos_x, pos_y)
+    cell_neighbours = neighbours(pos_x, pos_y)
+
+    case cell_neighbours
+    when 0, 1 then return 0
+    when 2
+      return 1 if cell(pos_x, pos_y) == 1
+    when 3 then return 1
+    when cell_neighbours > 3 then return 0
+    end
+
+    0
+  end
+
+  # evolve the board
+  def evolve
+    # store the new board in here
+    temp_board = Array.new(SIZE) { Array.new(SIZE, 0) }
+
+    (0...SIZE).each do |x|
+      (0...SIZE).each do |y|
+        temp_board[x][y] = live_or_die(x, y)
+      end
+    end
+
+    @board = temp_board
+  end
+
+  # run the program x times
+  def run(times = 2500, sleep_period = 0.1)
+    print "\e[2J\e[f"
+    times.times do |t|
+      puts "Generation #{t}"
+      show
+      evolve
+      sleep(sleep_period)
+      print "\e[2J\e[f"
+    end
+    show
   end
 end
 
 game = GameOfLife.new
-game.show
-puts game.neighbours(2, 0).inspect
+game.show # before the program starts
+game.run
